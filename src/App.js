a0939c1a2c317ba8085267f361c98fe98e6a1e57
Mohamed Gaber <122798757+mohamedgaber-136@users.noreply.github.com>
2023-10-27 14:50:09 +0200

    Delete mahmoudTest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MahmoudTest from "./MahmoudTest";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MahmoudTest />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
