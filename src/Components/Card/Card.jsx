cc2201b48d9087b10e5e7b5e286993f4717a906c
mohamedgaber-136 <mohamedd.gaberr.13696@gmail.com>
2023-10-29 17:48:14 +0200

    add animation to card and split form from header

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,25 +1,34 @@
 import bgimg from "./pexels-flo-dahm-699466.jpg";
 import "./Card.css";
-export const Card = () => {
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+export const Card = ({duration}) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div
-      className="cardParent py-2"
+      data-aos="fade-up"
+      data-aos-duration={`${duration}`}
+      className="cardParent py-2 animation"
       style={{
         backgroundImage: `url(${bgimg})`,
         backgroundPosition: "center",
         backgroundSize: "cover",
       }}
-      >
+    >
       <div className="d-flex  flex-column h-100  align-items-center justify-content-end">
-
-      <div className="cardData text-white container d-flex align-items-center justify-content-between">
-        <div className="title">
+        <div className="cardData text-white container d-flex align-items-center justify-content-between">
+          <div className="title">
             <h3 className="m-0">Paris</h3>
             <h6>An amazing journey</h6>
+          </div>
+          <div className="price">
+            <h3>600$</h3>
+          </div>
         </div>
-        <div className="price"><h3>600$</h3></div>
-      </div>
-      <button className="BookHotelBtn">Book a Hotel</button>
+        <button className="BookHotelBtn">Book a Hotel</button>
       </div>
     </div>
   );
