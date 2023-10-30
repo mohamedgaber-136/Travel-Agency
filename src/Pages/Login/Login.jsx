00bef9cd9dfc074b47e5e278f59d05a0f27eaccb
zolfa <unknown.zolfa@gmail.com>
2023-10-30 22:41:01 +0200

    edit login functions

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import img from "../../assets/photoLogin.png";
 
 import "./Login.css";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import { authbase } from "../../Firebase/FirebaseInit";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
@@ -14,15 +16,37 @@ function LoginPage() {
   };
 
   const [isHiddenPassword, setIsHiddenPassword] = useState("password");
+  const [userObject, setUserObject] = useState({
+    email: "",
+    password: "",
+    rememberMe: false,
+  });
 
   function handlePasswordType() {
     setIsHiddenPassword(!isHiddenPassword);
+  }
 
-    // if (passwordType === "password") {
-    //   setPassordType("text");
-    // } else {
-    //   setPassordType("password");
-    // }
+  const [credential, setCredential] = useState({});
+  useEffect(() => {
+    console.log(userObject, "user");
+    console.log(credential, "user");
+  }, [userObject, credential]);
+
+  function userSignIn(event) {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(authbase, userObject.email, userObject.password)
+      .then((cred) => {
+        console.log(cred, "sign in");
+        setCredential(cred);
+        // let x = { ...cred, phoneNumber: "+123456" };
+        // console.log(x, "x");
+        // authbase.currentUser = cred.user;
+        // add user from storage
+      })
+      .catch((error) => console.log(error, "error"));
+
+    console.log("ajsgs");
   }
 
   return (
@@ -37,12 +61,20 @@ function LoginPage() {
               backgroundSize: "cover",
             }}
           ></div>
+          <button
+            onClick={() => {
+              console.log(authbase.currentUser, "test");
+              authbase.currentUser.uid = "0";
+            }}
+          >
+            test
+          </button>
           <div className="loginForm h-100  ">
             <div className="align-self-start d-flex flex-column py-2">
               <h2 className="m-0">Login</h2>
               <span>Login to access your Golobe account</span>
             </div>
-            <form class="form">
+            <form class="form" onSubmit={userSignIn}>
               <div class="flex-column">
                 <label>Email </label>
               </div>
@@ -51,6 +83,9 @@ function LoginPage() {
                   type="text"
                   class="input"
                   placeholder="Enter your Email"
+                  onChange={(event) =>
+                    setUserObject({ ...userObject, email: event.target.value })
+                  }
                 />
               </div>
 
@@ -62,6 +97,12 @@ function LoginPage() {
                   type={isHiddenPassword ? "password" : "text"}
                   class="input"
                   placeholder="Enter your Password"
+                  onChange={(event) =>
+                    setUserObject({
+                      ...userObject,
+                      password: event.target.value,
+                    })
+                  }
                 />
                 <span
                   className="border border-dark px-2 py-1"
@@ -73,7 +114,15 @@ function LoginPage() {
 
               <div class="flex-row">
                 <div>
-                  <input type="checkbox" />
+                  <input
+                    type="checkbox"
+                    onChange={(event) =>
+                      setUserObject({
+                        ...userObject,
+                        rememberMe: !userObject.rememberMe,
+                      })
+                    }
+                  />
                   <label>Remember me </label>
                 </div>
                 <span class="span">Forgot password?</span>
