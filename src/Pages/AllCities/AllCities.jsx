fd292365995177f5bb6c6981c163a89dc88bc411
omar10ayman <omar.mo.ayman99@gmail.com>
2023-11-01 23:12:29 +0200

    addapiallcities

--- src/Pages/AllCities/AllCities.jsx
+++ src/Pages/AllCities/AllCities.jsx
@@ -7,21 +7,30 @@ import Loading from '../../Components/Loading/Loading'
 export default function AllCities() {
     const [cityImg,setCity]=useState([])
     useEffect(()=>{
-        getAllCities()
+        // getAllCities()
     },[])
     const getAllCities = async()=>{
         let arr=[]
-        const cities = all.map(city=>city.name)
-        const sliceCities = cities.slice(3000,3050)
-        for(let i=0; i<sliceCities.length; i++){
-            const res = await axios.get(`https://api.pexels.com/v1/search/?page=1&per_page=2&query=${i}`, {
+        const url = 'https://nomad-list-cities.p.rapidapi.com/nomad-list/europe?size=30&page=1&sort=desc&sort_by=overall_score';
+        const options = {
+	    method: 'GET',
+	    headers: {
+		'X-RapidAPI-Key': '872d9711d6msh649af9c6f912c09p1fa3ffjsn9fd6f1d47461',
+		'X-RapidAPI-Host': 'nomad-list-cities.p.rapidapi.com'
+	            }
+                        }; 
+        const res = await fetch(url, options)
+        const data = await res.json()
+        const cities = data.map(city=>city.name)
+        for(let i=0; i<cities.length; i++){
+            const res = await axios.get(`https://api.pexels.com/v1/search/?page=1&per_page=2&query=${cities[i]}`, {
                 headers: {
                     Authorization:
                     "dduIdgcxtb3X1K3WIllsexAvLU7spJTVWdd9ec9jbk2ii7qqWNQkCnBX",
                 },
             })
             console.log(res.data?.photos[0]?.src?.portrait)
-            arr.push({img:res.data?.photos[0]?.src?.original,title:sliceCities[i],id:Math.floor(Math.random()*1000)})
+            arr.push({img:res.data?.photos[0]?.src?.original,title:cities[i],id:Math.floor(Math.random()*1000)})
         }
         console.log(arr)
         setCity([...arr])
